b6da169ddbf59ad07b8d1bef4e4a1119da1585f4
code-with-zulquar <codewithzulquar@gmail.com>
2024-12-04 03:05:02 +0530

    upadate

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Page1 from "./Component/Page1";
+
+export default function App() {
+  return (
+    <>
+       <Page1/>   
+    
+    </>
+  )
+}
---
+++ src/Component/Page1.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Page1() {
+  return (
+    <div className="h-screen py-3 px-4 bg-white">
+      <div className="h-full shadow-xl shadow-gray-700 w-full bg-[url(https://static.wixstatic.com/media/f1c650_678c0ceab5194893872c60fa3be4bcdc~mv2.jpg/v1/fill/w_1316,h_572,fp_0.69_0.64,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/ANZO.jpg)] bg-cover rounded-[50px]"></div>
+    </div>
+  );
+}
+  
